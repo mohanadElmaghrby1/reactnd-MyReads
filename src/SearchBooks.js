85c4a6bd7f57597756cd389bad999ecda2b32cef
Mohannad <mohanad2021996@gmail.com>
2021-04-11 17:51:43 +0200

    Handle empty query

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import * as BooksAPI from './BooksAPI'
 import ListBooks from "./ListBooks";
 
@@ -28,31 +29,37 @@ class SearchBooks extends Component {
     }
 
     render() {
-        const books = this.state.books.map((book) => this.updateBookShelf(book));
+        let books = [];
+        if (!this.state.books.error)
+            books = this.state.books.map((book) => this.updateBookShelf(book));
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+                    <Link className="close-search" to='/'>Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                         <input type="text" placeholder="Search by title or author"
                             value={this.state.query}
                             onChange={(event) => this.updateQuery(event.target.value)} />
 
                     </div>
                 </div>
-                <div className="search-books-results">
-                    <ListBooks
-                        books={books}
-                        onUpdateBookShelf={this.props.onUpdateBookShelf} />
-                </div>
+
+                { books.length !== 0 ?
+                    (<div className="search-books-results">
+                        <ListBooks
+                            books={books}
+                            onUpdateBookShelf={this.props.onUpdateBookShelf} />
+                    </div >)
+                    : (<div className="search-books-results">
+                        <p>Use these terms Please: 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography',
+                        'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design',
+                        'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football',
+                        'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make',
+                        'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux',
+                        'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy',
+                          'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'</p>
+                    </div>)}
+
             </div>
         );
     }
